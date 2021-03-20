c77f9475f4147591cb85d35935916d904e54fc58
calledbo <calledbo@foxmail.com>
2021-03-20 22:41:09 +0800

    first upload

---
+++ microservice-hystrix-turbine/src/test/java/com/example/cloud/microservicehystrixturbine/MicroserviceHystrixTurbineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloud.microservicehystrixturbine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceHystrixTurbineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
